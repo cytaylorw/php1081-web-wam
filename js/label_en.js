e791dd113b44a73adf9e4e1acaf1db0275edb898
Taylor <cytaylorw@gmail.com>
2019-05-11 17:12:57 +0800

    新增英文翻譯

---
+++ js/label_en.js
@@ -0,0 +1,15 @@
+var label = {
+    title : "Save the Minions",
+    score : "Score: ",
+    start_button: "Start",
+    mode : "Mode: ",
+    mode_easy : "Easy",
+    mode_normal : "Normal",
+    mode_hard : "Hard",
+    mode_custom : "Custom",
+    total : "Round Time(second):",
+    num : "Number: ",
+    interval : "Interval(second): ",
+    remain_time : "Countdown(second): ",
+    result : `Time's up! You score {1} out of {0} total scores.`
+};
